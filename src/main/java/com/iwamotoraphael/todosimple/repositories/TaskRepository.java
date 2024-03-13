49262fbc843aaecf70b93e7c2cdccdb6181eb986
iwamotoraphael <iwamotoraphael@gmail.com>
2024-03-13 11:55:33 -0300

    feat: add task repository

---
+++ src/main/java/com/iwamotoraphael/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,23 @@
+package com.iwamotoraphael.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.iwamotoraphael.todosimple.models.Task;
+
+public interface TaskRepository extends JpaRepository<Task,Long>{
+    
+    //Underscore makes the query look for the id within the user
+    List<Task> findByUser_Id(Long id);
+
+    //Manual query with JPQL
+    //@Query(value= "SELECT t FROM Task t WHERE t.user.id = :id")
+    //List<Task> findByUserId(@Param("id") Long id);
+
+    //Manual Query with SQL
+    //@Query(value = "SELECT * FROM task t WHERE t.user_id = :id",nativeQuery = true)
+    //List<Task> findByUserId(@Param("id") Long id);
+}
