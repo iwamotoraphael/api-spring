688e6556e932003f0caf06684fce5aae07a4f620
iwamotoraphael <iwamotoraphael@gmail.com>
2024-03-13 12:52:37 -0300

    feat: add user service

---
+++ src/main/java/com/iwamotoraphael/todosimple/services/UserService.java
@@ -0,0 +1,49 @@
+package com.iwamotoraphael.todosimple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.iwamotoraphael.todosimple.models.User;
+import com.iwamotoraphael.todosimple.repositories.TaskRepository;
+import com.iwamotoraphael.todosimple.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+
+        return user.orElseThrow(() -> new RuntimeException("Usuário de id: "+id+" não foi encontrado."));
+    }
+
+    @Transactional
+    public User create(User obj){
+        obj.setId(null);
+        obj  = this.userRepository.save(obj);
+        
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User updatedObj  = findById(obj.getId());
+        updatedObj.setPassword(obj.getPassword());
+        
+        return this.userRepository.save(updatedObj);
+    }
+
+    public void delete(Long id){
+        this.taskRepository.deleteByUser_Id(id);
+        this.taskRepository.deleteById(id);
+    }
+}
+ 
