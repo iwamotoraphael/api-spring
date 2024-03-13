2443df334d4cccbc0fda05a48745447e8fe3b6af
iwamotoraphael <iwamotoraphael@gmail.com>
2024-03-13 16:43:42 -0300

    feat: add task service

---
+++ src/main/java/com/iwamotoraphael/todosimple/services/TaskService.java
@@ -0,0 +1,51 @@
+package com.iwamotoraphael.todosimple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.iwamotoraphael.todosimple.models.Task;
+import com.iwamotoraphael.todosimple.models.User;
+import com.iwamotoraphael.todosimple.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public Task findById(Long id){
+        Optional<Task> task = this.taskRepository.findById(id);
+
+        return task.orElseThrow(() -> new RuntimeException("Tarefa de id: "+id+" não foi encontrado."));
+    }
+
+    @Transactional
+    public Task create(Task task){
+        User user = this.userService.findById(task.getUser().getId());
+
+        task.setId(null);
+        task.setUser(user);
+        task = this.taskRepository.save(task);
+
+        return task;
+    }
+
+    @Transactional
+    public Task update(Task task){
+        Task updatedTask = findById(task.getId());
+
+        updatedTask.setDescription(task.getDescription());
+
+        return this.taskRepository.save(updatedTask);
+    }
+
+    public void delete(Long id){
+        this.taskRepository.deleteById(id);
+    }
+}
